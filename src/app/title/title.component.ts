c43614be892d70a71fed34b21ec43f30a97e5862
edson1609 <edson.1996.89@gmail.com>
2023-04-12 19:07:43 -0300

    life-cycle hooks  angular

---
+++ src/app/title/title.component.ts
@@ -0,0 +1,22 @@
+import { Component,Input,OnInit,OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.css']
+})
+export class TitleComponent implements OnInit,OnChanges {
+	@Input() nome: string = 'Pato';
+
+	constructor(){
+		console.log(`Construtor ${this.nome}`);
+	}
+	ngOnChanges(): void {
+		console.log(`OnChange ${this.nome}`);
+	}
+
+	ngOnInit(): void {
+		console.log(`OnInit ${this.nome}`);
+	}
+
+}
